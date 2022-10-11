245a1881e1a0f243c2f5d3184a53712072adcf54
yangguoneng <2228913483@qq.com>
2022-10-11 18:53:28 +0800

    第一个版本

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+    pubic static void main(String args[]){}
+    public static void version1(){} //vsrsion1
+    public static void version2(){} //vsrsion2
+
+    public static void version3(){  //version3
+         System.out.print("修复version3中的bug");
+    } 
+    public static void version4(){  //version4
+         System.out.println(version4()方法已经开发完毕);
+    }  
+}
